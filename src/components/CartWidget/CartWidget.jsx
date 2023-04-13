1321d666d3ccf0adde0c270f41a4e62bd96be669
nahuescudaletti <nahueby17@gmail.com>
2023-04-12 21:22:28 -0300

    commmmit

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/js/bootstrap.min.js" 
+
 import React from 'react'
 
 export const CartWidget = () => {
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/js/bootstrap.min.js" 
 import React from 'react'
 import CartWidget from '../CartWidget/CartWidget';
 export const NavBar = () => {
   return (
-    <div>
+    <div >
         <nav>
         <div class="nav-wrapper">
           <a href="#" class="brand-logo">MODA</a>
